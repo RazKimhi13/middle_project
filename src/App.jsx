9442e916ea3364b63f378903eab271d98fedfa3b
raz <razkimhi13@gmail.com>
2023-09-18 12:12:35 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css'
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import Blogs from "./components/Blogs/Blogs";
+import Contact from "./components/Contacts/Contacts";
+import NoPage from "./components/NoPage/NoPage";
 function App() {
 
   return (
     <>
- 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
