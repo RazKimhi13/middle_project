d6220c55b816696cb675ac72b0968e143a7033f5
Eden <edenmilo31@gmail.com>
2023-09-18 13:16:50 +0300

    contact update

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,10 @@
 const Contact = () => {
-    return <h1>Contact Me</h1>;
+    return
+    <div>
+       <h1>Contact Me</h1>
+      <h1>Eden milo</h1>
+       </div>
+
   };
   
   export default Contact;
